f2729002fd1c13441a346064c8061c1561d82b90
James <jameshaller27@gmail.com>
2021-12-13 20:49:40 -0800

    Make code classy

--- main.py
+++ main.py
@@ -1,35 +1,83 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as m_tick
+from functools import cached_property
+from typing import Optional, Iterator
 
 
-def get_total_effectiveness(effectiveness_list: list[float]) -> float:
-    total = 1
-    for e in effectiveness_list:
-        total *= (1 - e)
-    return 1 - total
+class Method:
+    _effectiveness: float
+
+    def __init__(self, effectiveness: float):
+        assert 0.0 <= effectiveness <= 1.0
+        self._effectiveness = effectiveness
+
+    @property
+    def effectiveness(self) -> float:
+        return self._effectiveness
+
+    @cached_property
+    def p_failure(self) -> float:
+        return 1 - self._effectiveness
 
 
-def get_cumulative_p_of_failure(e_total: float, recurrences: int) -> float:
-    return 1 - e_total**recurrences
+class Scenario:
+    _methods: list[Method]
+    _effectiveness: Optional[float]
+
+    def __init__(self):
+        self._methods = list()
+        self._effectiveness = None
+
+    def add_method(self, method: Method) -> None:
+        self._methods.append(method)
+
+    def effectiveness(self, n: int = 1) -> float:
+        if self._effectiveness is None:
+            total = 1
+            for m in self._methods:
+                total *= m.p_failure
+            self._effectiveness = 1 - total
+
+        return self._effectiveness ** n
+
+    def p_failure(self, recurrences: int = 1) -> float:
+        return 1 - self.effectiveness(recurrences)
+
+    def p_failure_list(self, *, min_times: int = 0, max_times: int) -> Iterator:
+        for n in range(min_times, max_times + 1):
+            yield self.p_failure(n)
+
+
+class MyPlot:
+    _ax: plt.Axes
+
+    def __init__(self):
+        self._ax = plt.axes()
+
+        self._ax.set_title("Probability of Pregnancy")
+        self._ax.grid(visible=True, which='major', linestyle='-')
+        self._ax.grid(visible=True, which='minor', axis='x', linestyle=':')
+        self._ax.minorticks_on()
+        self._ax.yaxis.set_major_formatter(m_tick.PercentFormatter(xmax=1))
+
+    def add_scenario(self, scenario: Scenario, window: tuple[int, int] = (0, 50)) -> None:
+        cumulative_p_list = list(scenario.p_failure_list(min_times=window[0], max_times=window[1]))
+        self._ax.plot(cumulative_p_list)
+
+    def show(self) -> None:
+        plt.show()
 
 
 def main():
-    effs = [0.99, 0.84]
-    e_total = get_total_effectiveness(effs)
+    plot = MyPlot()
 
-    cumulative_p_list = [get_cumulative_p_of_failure(e_total, n) for n in range(50+1)]
+    scenario = Scenario()
 
-    ax: plt.Axes = plt.axes()
+    scenario.add_method(Method(0.99))
+    scenario.add_method(Method(0.84))
 
-    ax.plot(cumulative_p_list)
-
-    ax.set_title("Probability of Pregnancy")
-    ax.grid(visible=True, which='major', linestyle='-')
-    ax.grid(visible=True, which='minor', axis='x', linestyle=':')
-    ax.minorticks_on()
-    ax.yaxis.set_major_formatter(m_tick.PercentFormatter(xmax=1))
-
-    plt.show()
+    plot.add_scenario(scenario)
+    plot.show()
 
 
 if __name__ == '__main__':
